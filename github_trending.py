d67d3b47e89712d1f30cfba4e61c09ee5564603e
Dimasik <prevetek@gmail.com>
2017-01-09 03:09:29 +0300

    fix issues
    Fix:
    - print format
    - repetition code
    - magic numbers
    - git commit

--- github_trending.py
+++ github_trending.py
@@ -3,24 +3,31 @@ import json
 from datetime import timedelta, datetime
 
 
-def get_trending_repositories(top_size=20):
-    seven_days_ago = datetime.now() - timedelta(7)
-    payload = {'q': 'created:>' + seven_days_ago.strftime("%Y-%m-%d"),
+def get_date_days_ago(days=0):
+    days_ago = datetime.now() - timedelta(days)
+    return days_ago.strftime("%Y-%m-%d")
+
+
+def get_trending_repositories(page_size=100):
+    date_week_ago = get_date_days_ago(7)
+    payload = {'q': 'created:>' + date_week_ago,
                'sort': 'stars',
-               'per_page': top_size}
+               'per_page': page_size}
     response = requests.get("https://api.github.com/search/repositories", params=payload)
     return json.loads(response.text)['items']
 
 
-def main():
-    repos = get_trending_repositories()
-    for repo in repos:
-        print('Repo name: ', repo['name'])
-        print('  URL:     ', repo['html_url'])
-        print('  Stars:   ', repo['stargazers_count'])
-        print('  Issues:  ', repo['open_issues'])
-        print()
+def printing(repositories):
+    for repo in repositories:
+        print('Repo name: {0}\n'
+              '  URL:     {1}\n'
+              '  Stars:   {2}\n'
+              '  Issues:  {3}\n'
+              ''.format(repo['name'], repo['html_url'], repo['stargazers_count'], repo['open_issues'])
+              )
 
 
 if __name__ == '__main__':
-    main()
+    page_size = 20
+    repositories = get_trending_repositories(page_size)
+    printing(repositories)
